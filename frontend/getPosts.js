834c85f35dfb7d82b8f7c4202391132a327f125e
Lu <luciasaenz92@hotmail.com>
2023-07-01 14:12:33 +0200

    basic layout front

--- frontend/getPosts.js
+++ frontend/getPosts.js
@@ -8,12 +8,18 @@ async function getPosts() {
         data.forEach(posts => {
             const limitBody = posts.body.substring(0, 50)
             const postContent = document.createRange().createContextualFragment(
-                `<article>                   
-                    <h2>${posts.title}</h2>
-                    <p>${limitBody} ... <a href="">Read More</a></p>
-                    <p>${posts.created}</p>
-                    <button>Delete</button>
-                </article>`
+                `
+                <div class="card  text-center mb-3 shadow" >
+                <img class="card-img-top">
+                <article class="card-body " >                   
+                    <h2 class="card-title">${posts.title}</h2>
+                    <p class="card-text">${limitBody} ... <a href="">Read More</a></p>                       
+                <div class="card-footer d-flex justify-content-between">     
+                <p>⏱ ${posts.created}</p>          
+                <button class="btn btn-secondary">🗑✒</button>
+                </div>  
+                </article>
+                </div>`
             )
 
             const postListElement = document.querySelector('#postList')
